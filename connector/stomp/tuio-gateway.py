0e2be07c20601d3c20ceb523397c232a0803f3ec
Fajran Iman Rusadi <fajran@gmail.com>
2009-06-28 21:07:02 +0200

    Add stomp producer. Currently only transmits events from fingers.

--- connector/stomp/tuio-gateway.py
+++ connector/stomp/tuio-gateway.py
@@ -33,24 +33,26 @@ try:
 
 		for c in tbr:
 			d = ld[c]
-			s.send("C R %d %f %f" % (c, d[1], d[2]), destination="/topic/home")
+			# cursor removed = 5
+			s.send("5 %d %d %f %f 0" % (c, c, d[1], d[2]), destination="/topic/home")
 			ld.pop(c)
 
 		# for c in tba:
 		# 	s.send("C N %d" % c, destination="/topic/home")
 
 		for c in t.cursors():
-			data = (c.sessionid, c.xpos, c.ypos)
+			data = (c.sessionid, c.sessionid, c.xpos, c.ypos)
 			if not ld.get(c.sessionid, None) == data:
 				if c.sessionid in tba:
-					msg = "C N %d %f %f" % data
+					# cursor new = 3
+					msg = "3 %d %d %f %f 0" % data
 				else:
-					msg = "C U %d %f %f" % data
+					# cursor update = 4
+					msg = "4 %d %d %f %f 0" % data
 				s.send(msg, destination="/topic/home")
 				ld[c.sessionid] = data
 				cnt += 1
-				print cnt
-				print "[cursor] [%d] (%f, %f) (%f, %f) %f" % (c.sessionid, c.xpos, c.ypos, c.xmot, c.ymot, c.mot_accel)
+
 except KeyboardInterrupt:
 	t.stop()
 
