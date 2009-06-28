c6bec53bfd6b913ad4a6cd5c918cb4b5e520f2b7
Fajran Iman Rusadi <fajran@gmail.com>
2009-06-28 20:59:21 +0200

    Add stomp-based connector (untested)

---
+++ connector/stomp/tuio.stomp.js
@@ -0,0 +1,50 @@
+
+tuio.connector.add('stomp', {
+
+	_host: 'localhost',
+	_port: 61613,
+	_user: 'guest',
+	_pass: 'guest',
+	_topic: '/topic/home',
+
+	_client: undefined,
+
+	init: function() {
+		var self = this;
+		var sc = new STOMPClient();
+		self._client = sc;
+
+		sc.onopen = function() {
+		}
+		sc.onclose = function(code) {
+		}
+		sc.onerror = function(err) {
+		}
+		sc.onerrorframe = function(frame) {
+		}
+		sc.onconnectedframe = function() {
+			sc.subscribe(self._topic, {exchange:''});
+		}
+		sc.onmessageframe = function(frame) {
+			self._process(frame.body.split(" "));
+		}
+		setTimeout(function() {
+			sc.connect(self._host, self._port, self._user, self._pass);
+		}, 10);
+	},
+
+	deinit: function() {
+		this._client.reset();
+	},
+
+	_process: function(data) {
+		var type = parseInt(data[0]);
+		var sid = parseInt(data[1]);
+		var fid = parseInt(data[2]);
+		var x = parseFloat(data[3]);
+		var y = parseFloat(data[4]);
+		var angle = parseFloat(data[5]);
+
+		tuio.callback(type, sid, fid, x, y, angle);
+	}
+});
